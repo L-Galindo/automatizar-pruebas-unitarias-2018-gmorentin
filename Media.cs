e7885fadf817e394031e9bea2a0d2e216747f3b1
Gustavo Fernando Morentin Ballesteros <gmorentin@ucol.mx>
2018-04-12 09:36:11 -0500

    Add files via upload

---
+++ Media.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo
+{
+    class Media
+    {
+        public static double mediaAritmetica(int[] vals)
+            {
+                double suma = 0;
+                double res = 0;
+                for (int cont = 0; cont < vals.Length; cont++)
+                {
+                    suma = suma + vals[cont];
+                }
+                res = suma / vals.Length;
+                return res;
+            }
+
+
+            private static double raizEnesima(double x, int n)
+            {
+            decimal num = (decimal)1 / n;
+                return Math.Pow(x,Convert.ToDouble(num));
+            }
+
+            public double mediaGeometrica(int[] vals)
+            {
+                double suma = 1;
+                for (int cont = 0; cont < vals.Length; cont++)
+                {
+                    suma = suma * vals[cont];
+                }
+                double res = raizEnesima(suma, vals.Length);
+                return res;
+            }
+
+            public static double mediaArmonica(int[] vals)
+            {
+                double suma = 0;
+                double res = 0;
+                for (int cont = 0; cont < vals.Length; cont++)
+                {
+                    suma = suma + ((double)1 / vals[cont]);
+                }
+                res = vals.Length / suma;
+                return res;
+            }
+        }
+    }
